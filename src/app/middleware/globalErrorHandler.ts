3edb6258e60d571345261d3f85b051e0e76f02c6
jonayeds <jonayeeds@gmail.com>
2024-12-07 12:26:09 +0600

    addded custom app error

--- src/app/middleware/globalErrorHandler.ts
+++ src/app/middleware/globalErrorHandler.ts
@@ -4,7 +4,7 @@
 import { NextFunction, Request, Response } from "express"
 
 export const globalErrorHandler =  (err:any,_req:Request, res:Response, next:NextFunction) => {
-    const statusCode =500
+    const statusCode = err.statusCode|| 500
     const message =  err.message || err?.errors[0].message  || "Something went wrong"
     res.status(statusCode).json({
       success:false,
--- src/app/modules/academicDepartment/academicDepartment.model.ts
+++ src/app/modules/academicDepartment/academicDepartment.model.ts
@@ -1,5 +1,6 @@
 import { model, Schema } from "mongoose";
 import { IAcademicDepartment } from "./academicDepartment.interface";
+import { AppError } from "../../errors/appError";
 
 const academicDepartmentSchema = new Schema<IAcademicDepartment>({
     name:{
@@ -25,9 +26,14 @@ academicDepartmentSchema.pre("findOneAndUpdate", async function name(next) {
     const query  = this.getQuery()
     const isDepartmentExist = await AcademicDepartment.findOne(query)
     if(!isDepartmentExist){
-        throw new Error("Department doesnot exist");
+        throw new AppError(404,"Department doesnot exist");
     }
     next()
 })
+
+
+
+
+
 
 export const AcademicDepartment = model<IAcademicDepartment>("AcademicDepartment", academicDepartmentSchema)
